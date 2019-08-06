fd049c529555d3d47a8d02dc871f0b4ebdeafde4
Dilshan <dilshanushara16@gmail.com>
2019-08-06 12:56:22 -0400

    Added a Splash Screen

---
+++ app/src/main/java/com/example/myevent/Splash.java
@@ -0,0 +1,32 @@
+package com.example.myevent;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash extends AppCompatActivity {
+
+    private static  int Splash=4000;
+
+    @RequiresApi(api = Build.VERSION_CODES.P)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i=new Intent(Splash.this,MainActivity.class);
+                startActivity(i);
+                finish();
+
+            }
+        },Splash);
+
+    }
+}
