f9c3dbcb0ebf4ca94650cd57a43cb4dbb507720a
Dilshan <dilshanushara16@gmail.com>
2019-08-06 03:20:20 -0400

    Updated Addevent2

--- app/src/main/java/com/example/myevent/MainActivity.java
+++ app/src/main/java/com/example/myevent/MainActivity.java
@@ -7,9 +7,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+
+
 public class MainActivity extends AppCompatActivity {
 
-
+    private Button b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +19,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        b=(Button) findViewById(R.id.button);
+
+        b.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                openaddevent2();
+            }
+
+
+        });
+
+
         }
 
+    private void openaddevent2() {
+
+        Intent i=new Intent(this, Addevent2.class);
+        startActivity(i);
     }
+
+}
 
 
 
