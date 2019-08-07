830a293f8e93d3354000718ff158024b83412adb
Dilshan <dilshanushara16@gmail.com>
2019-08-07 11:50:58 -0400

    Added a navigation drawer

--- app/src/main/java/com/example/myevent/Addevent2.java
+++ app/src/main/java/com/example/myevent/Addevent2.java
@@ -2,9 +2,14 @@ package com.example.myevent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Addevent2 extends AppCompatActivity {
+
+    private Button b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,7 +17,26 @@ public class Addevent2 extends AppCompatActivity {
         setContentView(R.layout.activity_addevent2);
 
 
+        b=(Button) findViewById(R.id.button2);
+
+        b.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                openafteraddevent();
+            }
+
+
+        });
+
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void openafteraddevent() {
+        Intent i=new Intent(this, Aftereventadded.class);
+        startActivity(i);
+
+
     }
 }
