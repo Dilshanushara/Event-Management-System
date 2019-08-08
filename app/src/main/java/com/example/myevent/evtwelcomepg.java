53df7179ee0ec6df9adea22b63a217c51d7e4b57
Aravinda <promodarvinda95@gmil.com>
2019-08-08 20:00:52 +0530

    navigationbar

--- app/src/main/java/com/example/myevent/evtwelcomepg.java
+++ app/src/main/java/com/example/myevent/evtwelcomepg.java
@@ -2,7 +2,9 @@ package com.example.myevent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class evtwelcomepg extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class evtwelcomepg extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_evtwelcomepg);
+    }
+
+
+    public void oncontinue(View view){
+        Intent intent=new Intent(this,evtnavdraw.class);
+
+        startActivity(intent);
+
     }
 }
