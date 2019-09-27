e22e1f6f4a44dc372572ff78811e3ba878f82e22
nilan45500 <avishkafernanado@gmail.com>
2019-09-27 22:20:05 +0530

    Post Ad Done

--- app/src/main/java/com/example/myevent/edit_ad.java
+++ app/src/main/java/com/example/myevent/edit_ad.java
@@ -1,28 +1,95 @@
 package com.example.myevent;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class edit_ad extends AppCompatActivity {
+
+    EditText txtName, txtDes, txtEmail, txtConNo;
+    Button btnSave, btnCancel;
+    DatabaseReference dbRef;
+    edit_ad1 std;
+
+    private void clearControls() {
+
+        txtName.setText("");
+        txtDes.setText("");
+        txtEmail.setText("");
+        txtConNo.setText("");
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_ad);
-    }
 
-    public void save_ad(View view) {
-        Intent intent = new Intent(this, select_ad.class);
+        txtName = findViewById(R.id.txtUpName);
+        txtDes = findViewById(R.id.txtUpDes);
+        txtEmail = findViewById(R.id.txtUpEmail);
+        txtConNo = findViewById(R.id.txtUpConNo);
 
-        startActivity(intent);
-    }
+        btnSave = findViewById(R.id.btnUpSave);
+        btnCancel = findViewById(R.id.btnUpCancel);
 
-    public void cancel(View view) {
-        Intent intent = new Intent(this, select_ad.class);
+        std = new edit_ad1();
 
-        startActivity(intent);
-    }
-}
+
+    Button buttonOne = (Button) findViewById(R.id.btnUpSave);
+        buttonOne.setOnClickListener(new Button.OnClickListener() {
+        public void onClick(final View v) {
+
+
+            DatabaseReference updRef = FirebaseDatabase.getInstance().getReference().child("edit_ad1");
+            updRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                    if (dataSnapshot.hasChild("Std1")) {
+
+                        try {
+                            std.setName(txtName.getText().toString().trim());
+                            std.setDescription(txtDes.getText().toString().trim());
+                            std.setEmail(txtEmail.getText().toString().trim());
+                            std.setConNo(Integer.parseInt(txtConNo.getText().toString().trim()));
+
+                            dbRef = FirebaseDatabase.getInstance().getReference().child("std");
+                            dbRef.setValue(std);
+                            clearControls();
+
+                            Toast.makeText(getApplicationContext(), "Data Updated Successfully", Toast.LENGTH_SHORT).show();
+
+                        } catch (NumberFormatException e) {
+                            Toast.makeText(getApplicationContext(), "Invalid Contact Number", Toast.LENGTH_SHORT).show();
+
+                        }
+                    } else
+                        Toast.makeText(getApplicationContext(), "No Source to Update", Toast.LENGTH_SHORT).show();
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+
+            });
+        }
+            private void openaftereditad() {           //create a method to open a the next activity
+                Intent i = new Intent(this, select_ad.class);
+                startActivity(i);
+        }
+
--- app/src/main/java/com/example/myevent/post_ad.java
+++ app/src/main/java/com/example/myevent/post_ad.java
@@ -34,55 +34,57 @@ public class post_ad extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_ad);
 
-//
-//        txtName = findViewById(R.id.EtName);
-//        txtDes = findViewById(R.id.EtDes);
-//        txtEmail = findViewById(R.id.EtEmail);
-//        txtConNo = findViewById(R.id.EtConNo);
-//
-//        btnSave = findViewById(R.id.btnPost);
-//        btnCancel = findViewById(R.id.cancel);
-//
-//        std = new edit_ad1();
-//
-//
-//        Button buttonOne = (Button) findViewById(R.id.btnPost);
-//        buttonOne.setOnClickListener(new Button.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
-//                Intent myIntent = new Intent(v.getContext(), select_ad.class);
-//                startActivity(myIntent);
-//
-//
-//                dbRef = FirebaseDatabase.getInstance().getReference().child("edit_ad1");
-//
-//                try {
-//                    if (TextUtils.isEmpty(txtName.getText().toString()))
-//                        Toast.makeText(getApplicationContext(), "Please enter Name", Toast.LENGTH_SHORT).show();
-//
-//                    else if (TextUtils.isEmpty(txtDes.getText().toString()))
-//                        Toast.makeText(getApplicationContext(), "Please enter Description", Toast.LENGTH_LONG).show();
-//
-//                    else if (TextUtils.isEmpty(txtEmail.getText().toString()))
-//                        Toast.makeText(getApplicationContext(), "Please enter Email", Toast.LENGTH_SHORT).show();
-//
-//                    else {
-//                        std.setName(txtName.getText().toString().trim());
-//                        std.setDescription(txtDes.getText().toString().trim());
-//                        std.setEmail(txtEmail.getText().toString().trim());
-//                        std.setConNo(Integer.parseInt(txtConNo.getText().toString().trim()));
-//
-//                        dbRef.push().setValue(std);
-//
-//                        Toast.makeText(getApplicationContext(), "Posted", Toast.LENGTH_SHORT).show();
-//                    }
-//                } catch (NumberFormatException e) {
-//                    Toast.makeText(getApplicationContext(), "Invalid Contact Number", Toast.LENGTH_SHORT).show();
-//                }
-//            }
-//
-//
-//        });
-       }
-       }
+
+        txtName = findViewById(R.id.EtName);
+        txtDes = findViewById(R.id.EtDes);
+        txtEmail = findViewById(R.id.EtEmail);
+        txtConNo = findViewById(R.id.EtConNo);
+
+        btnSave = findViewById(R.id.btnPost);
+        btnCancel = findViewById(R.id.cancel);
+
+        std = new edit_ad1();
+
+
+        Button buttonOne = (Button) findViewById(R.id.btnPost);
+        buttonOne.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                dbRef = FirebaseDatabase.getInstance().getReference().child("edit_ad1");
+
+                try {
+                    if (TextUtils.isEmpty(txtName.getText().toString()))
+                        Toast.makeText(getApplicationContext(), "Please enter Name", Toast.LENGTH_SHORT).show();
+
+                    else if (TextUtils.isEmpty(txtDes.getText().toString()))
+                        Toast.makeText(getApplicationContext(), "Please enter Description", Toast.LENGTH_LONG).show();
+
+                    else if (TextUtils.isEmpty(txtEmail.getText().toString()))
+                        Toast.makeText(getApplicationContext(), "Please enter Email", Toast.LENGTH_SHORT).show();
+
+                    else {
+                        std.setName(txtName.getText().toString().trim());
+                        std.setDescription(txtDes.getText().toString().trim());
+                        std.setEmail(txtEmail.getText().toString().trim());
+                        std.setConNo(Integer.parseInt(txtConNo.getText().toString().trim()));
+
+                        dbRef.push().setValue(std);
+
+                        Toast.makeText(getApplicationContext(), "Posted", Toast.LENGTH_SHORT).show();
+                    }
+                } catch (NumberFormatException e) {
+                    Toast.makeText(getApplicationContext(), "Invalid Contact Number", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+
+        });
+    }
+
+    private void openafterpostad() {           //create a method to open a the next activity
+        Intent i = new Intent(this, select_ad.class);
+        startActivity(i);
+    }
+
+}
