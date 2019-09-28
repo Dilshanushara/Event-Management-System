416a417f1e5acbc0ba1ec3cc16671e1dfb9f81d1
Gayan <gayanpj1996@gmail.com>
2019-09-28 09:01:28 +0530

    Update Firebase in Customer

---
+++ app/src/main/java/com/example/myevent/Common/Common.java
@@ -0,0 +1,9 @@
+package com.example.myevent.Common;
+
+import com.example.myevent.Customer.customer;
+import com.example.myevent.Model.EventPlanner;
+
+public class Common {
+    public static EventPlanner currenteventplanner;
+    public static customer  currcustomer;
+}
